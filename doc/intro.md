0b4893cd128a8ef890b29b00f83a1534740e5eb0
Carin Meier <cmeier@gigasquidsoftware.com>
2014-02-28 22:19:33 -0500

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to robot-dance-party
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/robot_dance_party/core_test.clj
@@ -0,0 +1,7 @@
+(ns robot-dance-party.core-test
+  (:require [clojure.test :refer :all]
+            [robot-dance-party.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
