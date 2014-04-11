c39fb513ee6ab164749f32d8882d4fac11c08611
Carin Meier <cmeier@gigasquidsoftware.com>
2014-04-10 21:57:21 -0400

    restructuring of sphero and roomba

--- src/robot_dance_party/core.clj
+++ src/robot_dance_party/core.clj
@@ -7,61 +7,69 @@
             [ellipso.commands :as commands])
   (:import roombacomm.RoombaCommSerial))
 
+(declare roomba)
+(declare sphero)
+
 (comment
-(def roomba (RoombaCommSerial. ))
+  ;; init the roomba
+  (def roomba (RoombaCommSerial. ))
 
-;;Find your port for your Roomba
-(map println (.listPorts roomba))
+  ;;Find your port for your Roomba
+  (map println (.listPorts roomba))
 
-(def portname "/dev/rfcomm0")
-(.connect roomba portname)
-(.startup roomba)  ;;puts Roomba in safe Mode
-;; What mode is Roomba in?
-(.modeAsString roomba)
-(.control roomba)
-(.updateSensors roomba) ; returns true if you are connected
-(.playNote roomba 72 40)
-(.disconnect roomba)
+  (def portname "/dev/rfcomm0")
+  (.connect roomba portname)
+  (.startup roomba)  ;;puts Roomba in safe Mode
+  ;; What mode is Roomba in?
+  (.modeAsString roomba)
+  (.control roomba)
+  (.updateSensors roomba) ; returns true if you are connected
+  (.playNote roomba 72 40)
+  (.disconnect roomba)
 
-  
+  ;; init the sphero
   (def sphero (core/connect "/dev/rfcomm1"))
 
   (core/disconnect sphero)
 
   (commands/execute sphero (commands/colour 0xFF0000)) ;;red
   (commands/execute sphero (commands/colour 0xFF8000)) ;;yellow
-  (commands/execute sphero (commands/colour 0x0000FF)) ;;blue
-  (commands/execute sphero (commands/colour 0xFF00FF))) ;;purple
+) 
+
+(def RED 0xFF0000)
+(def YELLOW 0xFF8000)
+(def BLUE 0x0000FF)
+(def PURPLE 0xFF00FF)
 
 
-(def rainbow
-  (map commands/colour [0xFF0000 0xFF8000 0xFFFF00 0x00FF00 0x0000FF 0x8000FF 0xFF00FF]))
-(def sphero-moves
-  [(commands/roll 0x4B 0) (commands/roll 0x4B 180)])
+(def sphero-moves (atom []))
+(defn change-sphero-moves [moves] (reset! sphero-moves moves))
 
 (defn sphero-loop-player
-  [beat colors angle count angle-incr]
-  (let [next-beat (+ 4 beat)
-        new-angle (if (< angle 360) (+ angle 360) angle)
-        new-angle-incr (if (zero? (mod count 12)) (* angle-incr -1) angle-incr)
-        good-angle (if (zero? (mod count 12)) (* new-angle -1) new-angle)]
+  [beat movelist]
+  (let [next-beat (+ 16 beat)
+        moves (first movelist)
+        next-moves (or (rest movelist) @sphero-moves)]
     (at (metro beat)
-        (commands/execute sphero (first colors))
-        (commands/execute sphero (commands/roll 0x4B (mod good-angle 360)))
-        ;(commands/execute sphero (first moves))
-        )
-    (apply-at (metro next-beat) #'sphero-loop-player [next-beat (rest colors) (+ good-angle new-angle-incr) (inc count) new-angle-incr])))
+        (when moves
+          (if (seq? moves)
+           (doseq [move moves]
+             (commands/execute sphero move))
+           (commands/execute sphero moves))))
+    (apply-at (metro next-beat) #'sphero-loop-player [next-beat next-moves])))
+
+(def roomba-moves (atom []))
+(defn change-roomba-moves [move-list] (reset! roomba-moves move-list))
 
 (defn roomba-loop-player
-  [beat turn]
-  (let [next-beat (+ 8 beat)]
+  [beat movelist]
+  (let [next-beat (+ 8 beat)
+        move (first movelist)
+        next-moves (or (rest movelist) @roomba-moves)]
     (at (metro beat)
-        (if turn
-          (.spinLeft roomba)
-          (.spinRight roomba))
-        )
-    (.playNote roomba 72 40)
-    (apply-at (metro next-beat) #'roomba-loop-player [next-beat (not turn)])))
+        (when move
+          (move roomba)))
+    (apply-at (metro next-beat) #'roomba-loop-player [next-beat next-moves])))
 
 
 (defn go-play [beat song]
@@ -69,8 +77,8 @@
     (reset! next-melody song)
     (bass-loop-player m)
     (robot-loop-player m)
-    (roomba-loop-player m true)
-    (sphero-loop-player m (cycle rainbow) 0 0 30)
+    (roomba-loop-player m @roomba-moves)
+    (sphero-loop-player m @sphero-moves)
     (melody-loop-player m song @next-melody)))
 
 
@@ -78,31 +86,6 @@
   (stop)
   (.stop roomba)
   (commands/execute sphero (commands/roll 0x00 180)))
-
-(comment  (all-stop)
-
-          (commands/execute sphero (commands/roll 0x30 0))
-          (commands/execute sphero (commands/roll 0x00 0))
-          (commands/execute sphero (commands/roll 0x30 180))
-          (commands/execute sphero (commands/roll 0 180))
-
-          (commands/execute sphero (commands/roll 0x64 0))
-          (commands/execute sphero (commands/roll 0x64 180))
-
-          (commands/execute sphero (commands/roll 0x00 180))
-
-          (commands/execute sphero (commands/roll 0x30 0))
-          (commands/execute sphero (commands/roll 0x30 45))
-          (commands/execute sphero (commands/roll 0x30 90))
-          (commands/execute sphero (commands/roll 0x30 135))
-          (commands/execute sphero (commands/roll 0x30 180))
-          (commands/execute sphero (commands/roll 0x30 225))
-          (commands/execute sphero (commands/roll 0x30 270))
-          (commands/execute sphero (commands/roll 0x30 315))
-          (commands/execute sphero (commands/roll 0x30 0))
-
-          )
-
 
 
 (comment
@@ -127,7 +110,7 @@
   (change-melody repetition-a)
 
 
-  (change-bass-sounds [daft-kick])
+  (change-bass-sounds [dirty-kick])
   (change-melody-sounds [])
 
   (change-melody-sounds [piano])
@@ -136,7 +119,12 @@
   (change-melody-sounds [])
 
   (change-bass-sounds [dirty-kick daft-kick])
-  (change-melody-sounds [plucked-string])
+  (change-melody-sounds [piano])
+
+  (change-roomba-moves [ #(.spinRight %) #(.spinLeft %)])
+  (change-sphero-moves (map commands/colour [RED YELLOW BLUE PURPLE]))
+  (change-sphero-moves [(commands/roll 0x4B 0) (commands/roll 0x4B 180)])
+
 
   (all-stop))
 
